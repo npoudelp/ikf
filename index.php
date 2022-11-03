82e879f02e764ca3c9c2eae708391f21c0fa8c85
npoudelp <npoudelp@gmail.com>
2022-11-03 22:03:48 +0545

    wireframe for imdex page designed

---
+++ index.php
@@ -0,0 +1,99 @@
+<head>
+    <title>IKF-Home</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/jQuery.js"></script>
+</head>
+
+<body class="bg-dark text-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="home">
+        <div class="container">
+            <a class="nav-link text-warning h1" href="#">I.K.F</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mx-5 font lead" id="navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#about">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#developer">About Developer</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section -->
+    <section class="lead py-3" id="home">
+        <div class="mx-0">
+            <div class="row bg-white">
+                <div class="col md-6">
+                    <img src="./images/cow.gif" class="mx-5 img-fluid w-75 d-none d-md-block">
+                </div>
+                <div class="col md-6 h-100 align-middle text-center">
+                    <p class="py-5 display-1 text-warning border-bottom border-warning">
+                        Indira Krishi Farm
+                    </p>
+                    <p class="lead text-dark">
+                        bla bla bla
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- about us section -->
+    <section class="lead p-3" id="about">
+        <div class="container">
+            <p class="py-2 text-center display-2 text-warning">About Us</p>
+            <div class="row">
+                <div class="col-md-3 shadow p-3 mx-5">
+                    <img src="./images/cow.gif" class="w-100 img-thumbnail mx-auto d-block" alt="">
+                    <p class="text-center">Hello this is center of text. Bla bla bla</p>
+                </div>
+                <div class="col-md-3 shadow p-3 mx-5">
+                    <img src="./images/cow.gif" class="w-100 img-thumbnail mx-auto d-block" alt="">
+                    <p class="text-center">Hello this is center of text. Bla bla bla</p>
+                </div>
+                <div class="col-md-3 shadow p-3 mx-5">
+                    <img src="./images/cow.gif" class="w-100 img-thumbnail mx-auto d-block" alt="">
+                    <p class="text-center">Hello this is center of text. Bla bla bla</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact us section -->
+    <section class="lead p-3" id="contact">
+        <div class="mx-0">
+            <div class="row bg-white text-dark">
+                <div class="col md-6 p-5">
+
+                </div>
+                <div class="col md-6 align-middle text-center">
+                    <iframe class="w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5473.1210184147185!2d87.61752150310315!3d26.651594694698804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa61bb7500ad36aa8!2sIndira%20krishi%20Farm!5e1!3m2!1sen!2snp!4v1667491746472!5m2!1sen!2snp" allowfullscreen="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- about developer section -->
+    <section class="lead p-3" id="developer">
+
+    </section>
+
+    <!-- footer section -->
+    <section class="lead p-3" id="footer">
+
+    </section>
+
+</body>
